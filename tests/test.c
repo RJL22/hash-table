67c6d22a5722af1d9d74fd0dc4f2c0e90144b2c7
RJL22 <ryanjlee22@gmail.com>
2021-06-09 19:40:36 -0400

    Update test run file

--- tests/test.c
+++ tests/test.c
@@ -6,6 +6,17 @@ int main() {
 
 	ht_hash_table* hashTable = new_hashtable();
 
+	ht_insert(hashTable, "A", "Alpha");
+	ht_insert(hashTable, "B", "Beta");
+	ht_insert(hashTable, "G", "Gamma");
+
+	const char* item1 = ht_search(hashTable, "A");
+	const char* item2 = ht_search(hashTable, "B");
+	const char* item3 = ht_search(hashTable, "G");
+
+	printf("%s\n", item1);
+	printf("%s\n", item2);
+	printf("%s\n", item3);
 
 	return 0;
 }
